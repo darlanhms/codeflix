e593f78577d440fe5eeeff2fb327fed4758251c7
Darlan H <darlanhermessjb@hotmail.com>
2022-05-05 23:19:33 -0300

    feat: initial category domain props

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,9 +1,19 @@
-import {Category} from './category'
+import { Category } from './category';
 
-describe("Category tests", () => {
-    it("constructor of category", () => {
-        const category = new Category("Movie")
+describe('Category tests', () => {
+    it('constructor of category', () => {
+        const props = {
+            name: 'Movie',
+            description: 'some description',
+            is_active: true,
+            created_at: new Date(),
+        };
 
-        expect(category.name).toBe("Movie")
-    })
-})
+        const category = new Category(props);
+
+        expect(category.name).toBe('Movie');
+        expect(category.description).toBe('some description');
+        expect(category.is_active).toBeTruthy();
+        expect(category.created_at).toBe(props.created_at);
+    });
+});
